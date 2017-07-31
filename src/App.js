f6d15463bb3c5737ccd523b621b9572992772959
Emiliano Mateu <mateuemiliano@gmail.com>
2017-07-31 18:15:38 -0300

    Style updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { injectGlobal } from 'styled-components'
 import { normalize } from 'polished'
 import { Grid, GridResponsive } from './components/PureCSS'
+import { fontFamily } from './styled'
 import Home from './scenes/Home'
 
 /* eslint-disable */
@@ -9,6 +10,17 @@ injectGlobal`
   ${normalize()}
   ${Grid}
   ${GridResponsive}
+
+  html, .pure-g [class *= "pure-u"] {
+    font-family: ${fontFamily};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  ::selection {
+    color: #fff;
+    background: #26a6d6;
+  }
 `
 /* eslint-enable */
 
